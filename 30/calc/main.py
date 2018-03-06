9bc032279e02f4d38a659d358231bfad556f306e
Tomáš Derner <tomasderner97@gmail.com>
2018-03-06 18:55:25 +0100

    30: done

--- 30/calc/main.py
+++ 30/calc/main.py
@@ -1,6 +1,7 @@
 from protokol import *
 
 LAMBDA = (588.9950 + 589.5924) / 2 * 1e-9
+N_SKLA = 1.523
 
 # ----- ÚKOL 1 ----- #
 
@@ -64,11 +65,57 @@ def plot_newton(show=False, save=False):
     print(fit_A1.params)
     print(fit_A1.errors)
 
+    # ---
+    fit_A2 = FitCurve(
+        f_line,
+        newton.index + 1,
+        newton["sn4"]**2 / LAMBDA
+    )
+
+    ax.plot(*fit_A2.curve(), ":", c="grey")
+    ax.plot(fit_A2.xdata, fit_A2.ydata, "k+", label="A2")
+
+    print(fit_A2.params)
+    print(fit_A2.errors)
+
+    # ---
+    fit_B1 = FitCurve(
+        f_line,
+        newton.index + 1,
+        newton["sn3"]**2 / LAMBDA
+    )
+
+    ax.plot(*fit_B1.curve(), "--", c="grey")
+    ax.plot(fit_B1.xdata, fit_B1.ydata, "ko", label="B1")
+
+    print(fit_B1.params)
+    print(fit_B1.errors)
+
+    # ---
+    fit_B2 = FitCurve(
+        f_line,
+        newton.index + 1,
+        newton["bn3"]**2 / LAMBDA
+    )
+
+    ax.plot(*fit_B2.curve(), "-.", c="grey")
+    ax.plot(fit_B2.xdata, fit_B2.ydata, "k^", label="B2")
+
+    print(fit_B2.params)
+    print(fit_B2.errors)
+
+    # ---
+
+    ax.set_xlabel(r"$k[1]$")
+    ax.set_ylabel(r"$\frac{\rho_k^2}{\lambda} [\si{m}]$")
+    ax.legend()
+    fig.tight_layout()
+
     if show:
         plt.show()
 
     if save:
-        fig.savefig("../plot/vrstva1.pdf")
+        fig.savefig("../plot/newton.pdf")
 
 # ----- ÚKOL 3 ----- #
 
@@ -82,9 +129,24 @@ def plot_kalibrace():
 
     fit = FitCurve(f_line, (kalibrace.index + 1) * 100, kalibrace[0])
     print(fit.params[0], fit.errors[0])
-    ax.plot((kalibrace.index + 1) * 100, kalibrace[0])
+    ax.plot((kalibrace.index + 1) * 100, kalibrace[0], "-+k")
     ax.set_xlabel("Skutečná hodnota $[\si{\micro\metre}]$")
     ax.set_ylabel("Hodnota zobrazená počítačem $[\si{\micro\metre}]$")
     plt.tight_layout()
     plt.savefig("../plot/kalibrace.pdf")
     # plt.show()
+
+# ----- ÚKOL 4 ----- #
+
+
+def mohutnost(r1, r2):
+    return (N_SKLA - 1) * (1 / r1 + 1 / r2)
+
+
+r_1a = uf(0.073, 0.001)
+r_1b = uf(0.079, 0.001)
+r_2a = uf(0.097, 0.001)
+r_2b = uf(0.048, 0.001)
+
+moh_a = mohutnost(r_1a, r_1b)
+moh_b = mohutnost(r_2a, r_2b)
